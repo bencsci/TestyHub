d4b062555e7024576f491b903d39fd6d27bef9e2
Ben Le <127059162+bencsci@users.noreply.github.com>
2025-06-29 20:21:54 -0300

    Solved 'has path' (python3)

--- Graph-I/078-has-path/has-path.py
+++ Graph-I/078-has-path/has-path.py
@@ -1,14 +1,13 @@
-from collections import deque
-
 def has_path(graph, src, dst):
-  q = deque([src])
 
-  while q:
-    curr = q.popleft()
-    if curr == dst:
+  def dfs(src):
+    if src == dst:
       return True
-    for neighbour in graph[curr]:
-      q.append(neighbour)
 
-  return False
-      
+    for node in graph[src]:
+      if dfs(node):
+        return True
+
+    return False
+
+  return dfs(src)
