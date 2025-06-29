d60854f14b6066fceb8f95b6aac1de4975a57568
Ben Le <127059162+bencsci@users.noreply.github.com>
2025-06-29 20:23:33 -0300

    Solved 'linked list values' (python3)

---
+++ Linked-List-I/036-linked-list-values/linked-list-values.py
@@ -0,0 +1,12 @@
+# class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.next = None
+
+def linked_list_values(head):
+  ans = []
+  curr = head
+  while curr:
+    ans.append(curr.val)
+    curr = curr.next 
+  return ans
